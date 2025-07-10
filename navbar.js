4e01fc123a03dc0a3fac01f233475e1f22d198aa
notmeimei <meimyang@yahoo.ca>
2025-07-09 21:08:48 -0400

    remove home button.js

--- navbar.js
+++ navbar.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const nav = document.querySelector('header nav');
   if (nav) {
     nav.innerHTML = `
-      <button onclick="window.location.href='index.html'">Home</button>
       <button onclick="window.location.href='cv.html'">CV</button>
       <button onclick="window.location.href='exhibitions.html'">Exhibitions</button>
       <button onclick="window.location.href='https://www.instagram.com/cmilanb/?hl=en'">Instagram</button>
